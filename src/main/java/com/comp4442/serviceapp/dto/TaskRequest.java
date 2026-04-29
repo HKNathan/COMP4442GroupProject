f9e1f09b76364d2868f20a0521d511bddd36be0a
Carsonyam <carsonyam326@gmail.com>
2026-04-29 21:41:26 +0800

    add TaskRequest DTO for expression input

---
+++ src/main/java/com/comp4442/serviceapp/dto/TaskRequest.java
@@ -0,0 +1,10 @@
+package com.comp4442.serviceapp.dto;
+
+public class TaskRequest {
+    private String expression;
+
+    public TaskRequest() {}
+
+    public String getExpression() { return expression; }
+    public void setExpression(String expression) { this.expression = expression; }
+}
